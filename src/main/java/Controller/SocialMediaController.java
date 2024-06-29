e1329061b794c0d6aefef572b50bcd4a9ea99a72
Chinedu Ezuma <chineduezuma@yahoo.co.uk>
2024-06-29 19:16:31 +0000

    SocialMedia Project

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,7 +1,15 @@
 package Controller;
 
+import Model.Account;
+import Model.Message;
+import Service.AccountService;
+import Service.MessageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.util.List;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +17,17 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+
+    AccountService accountService;
+    MessageService messageService;
+
+    public SocialMediaController() {
+        this.accountService = new AccountService();
+        this.messageService = new MessageService();
+    }
+
+
+
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -17,7 +36,9 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
-
+        app.post("/register", this::postNewUserHandler);
+        app.post("/login", this::postVerifyUserHandler);
+        app.post("/messages", this::postNewMessagesHandler);
         return app;
     }
 
@@ -27,6 +48,72 @@ public class SocialMediaController {
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+
+
+    /**
+     * Handler to post a new user account.
+     * The Jackson ObjectMapper will automatically convert the JSON of the POST request into an Account object.
+     * If AccountService returns a null account (meaning posting an Account was unsuccessful), the API will return a 400
+     * message (client error). 
+     * @param ctx the context object handles information HTTP requests and generates responses within Javalin. It will
+     *            be available to this method automatically thanks to the app.post method.
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void postNewUserHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = accountService.addAccount(account);
+        if(addedAccount!=null){
+            ctx.json(mapper.writeValueAsString(addedAccount));
+        }else{
+            ctx.status(400);
+        }
+    }
+
+
+
+     /**
+     * Handler to verify / authenticate an account.
+     * The Jackson ObjectMapper will automatically convert the JSON of the POST request into an Account object.
+     * If AccountService returns a null account (meaning posting an Account was unsuccessful), the API will return a 400
+     * message (client error). 
+     * @param ctx the context object handles information HTTP requests and generates responses within Javalin. It will
+     *            be available to this method automatically thanks to the app.post method.
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void postVerifyUserHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account verifiedAccount = accountService.verifyAccountLogin(account);
+        if(verifiedAccount!=null){
+            ctx.json(mapper.writeValueAsString(verifiedAccount));
+        }else{
+            ctx.status(401);
+        }
+    }
+
+
+
+      /**
+     * Handler to verify / authenticate an account.
+     * The Jackson ObjectMapper will automatically convert the JSON of the POST request into an Account object.
+     * If AccountService returns a null account (meaning posting an Account was unsuccessful), the API will return a 400
+     * message (client error). 
+     * @param ctx the context object handles information HTTP requests and generates responses within Javalin. It will
+     *            be available to this method automatically thanks to the app.post method.
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void postNewMessagesHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Message message = mapper.readValue(ctx.body(), Message.class);
+        Message addedNewMessage = messageService.addMessage(message);
+        if(addedNewMessage!=null){
+            ctx.json(mapper.writeValueAsString(addedNewMessage));
+        }else{
+            ctx.status(400);
+        }
     }
 
 
